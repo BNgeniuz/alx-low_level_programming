b38abecede17d4785d85afbc3e7695f78bf4a06b
BNgeniuz <godwin.bruce@gmail.com>
2023-07-13 14:51:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code to print 'puts' function
+ * return: 0 (Good)
+ */
+int main(void)
+{
+print("\"Programming is like building a multilingual puzzle");
+return (0);
+}
