9990d70f9ff043988031cd2e44ea53e049e57a02
BNgeniuz <godwin.bruce@gmail.com>
2023-07-13 15:25:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - write a code to print f function
+ * Return: 0 (Perfect)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
