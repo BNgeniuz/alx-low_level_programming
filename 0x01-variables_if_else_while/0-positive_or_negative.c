5c2c8c8015159eb0435c0653dcf1b7c268c99158
BNgeniuz <godwin.bruce@gmail.com>
2023-07-14 12:10:15 -0700

    0-positive_or_negative.c:10: warning: No description found for return value of 'main'

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,25 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Return : Always 0 (Perfect)
- */
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * maain - Entry point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%d is zero\n,", n);
 	else
 		printf("%d is negative\n", n);
 	return (0);
