91acbb673fafec4db78c8f81dd6736789804af5f
BNgeniuz <godwin.bruce@gmail.com>
2023-07-14 16:54:07 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - all single digit numbers of base 10 starting from 0
+*
+* Return: 0 (Perfect)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(1 + 'i');
+	putchar('\n');
+	return (0);
+}
