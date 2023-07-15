b260104cabd2ec2d01127f81bf8b7aef6f9e06d0
BNgeniuz <godwin.bruce@gmail.com>
2023-07-14 17:37:08 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 'z' ; h >= 'z' ; h++)
+		putchar(h);
+	putchar('\n');
+	return (0);
+}
