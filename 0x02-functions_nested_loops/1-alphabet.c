03e5947bf0b31b56318fadb0435efe61227b450b
BNgeniuz <godwin.bruce@gmail.com>
2023-07-18 09:30:19 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower cases
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+	char cr;
+
+	for (cr = 'a' ; cr <= 'z' ; cr++)
+		_putchar(cr);
+	_putchar('\n');
+}
