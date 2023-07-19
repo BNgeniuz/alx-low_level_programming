416c7036cec2d20aaba3fc1eb2e04c9ce913f0e6
BNgeniuz <godwin.bruce@gmail.com>
2023-07-18 22:24:18 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: (0) natural numbers
+ */
+int main(void)
+{
+	int z, sum = 0;
+
+	for (z = 0; z < 1024; z++)
+	{
+		if ((z % 5 == 0) || (z % 3 == 0))
+			sum + = z;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
