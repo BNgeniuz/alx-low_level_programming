db64143acb0a771f3ead5b3a95575a70fbaac3bf
BNgeniuz <godwin.bruce@gmail.com>
2023-07-19 05:06:04 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,20 +5,33 @@
  */
 int main(void)
 {
-	int q = 0;
-	unsigned long int x = 0, y = 1, z = 0;
+	unsigned long int x, y, z, y1, y2, z1, z2;
 
-	while (q < 98)
+	y = 1;
+	z = 2;
+
+	printf("%lu", y);
+
+	for (x = 1; x < 91; x++)
 	{
-		z = x + y;
-		x = y;
-		y = z;
-		printf("%lu", z);
-
-		if (q < 91)
-			printf(", ");
-		q++;
+		printf(", %lu", z);
+		z = z + y;
+		y = z - y;
 	}
-	putchar('\n');
+	y1 = y / 1000000000;
+	y2 = y % 1000000000;
+	z1 = z / 1000000000;
+	z2 = z % 1000000000;
+
+	for (x = 92; x < 99; x++)
+	{
+		printf(", %lu", z1 + (z2 / 1000000000));
+		printf("%lu", z2 % 1000000000);
+		z1 = z1 + y1;
+		y1 = z1 - y1;
+		z2 = z2 + y2;
+		y2 = z2 - y2;
+	}
+	printf("\n");
 	return (0);
 }
