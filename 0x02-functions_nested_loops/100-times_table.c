efcfe9942fb8b2c021f2815120328081da5adfed
BNgeniuz <godwin.bruce@gmail.com>
2023-07-18 22:08:11 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,37 @@
 #include "main.h"
 /**
  * print_times_table - nth time table starting with 0
- * @i: deduce parameter
+ * @n: deduce parameter
  * Return: (0) nth times table
  */
-void print_times_table(int i)
+void print_times_table(int n)
 {
 	int e, k, v;
 
-	if (i <= 15 && i >= 0)
-	for (e = 0; e <= i; e++)
-	{
-		_putchar('0');
-		for (k = 1; k <= i; k++)
+	if (n <= 15 && n >= 0)
+		for (e = 0; e <= n; e++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			v = e * k;
-			if (v <= 99)
-				_putchar(' ');
-			if (v <= 9)
-				_putchar(' ');
-			if (v >= 100)
+			_putchar('0');
+			for (k = 1; k <= n; k++)
 			{
-				_putchar((v / 100) + '0');
-				_putchar(((v / 10) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				v = e * k;
+				if (v <= 99)
+					_putchar(' ');
+				if (v <= 9)
+					_putchar(' ');
+				if (v >= 100)
+				{
+					_putchar((v / 100) + '0');
+					_putchar(((v / 10) % 10) + '0');
+				}
+				else if (v <= 99 && v >= 10)
+				{
+					_putchar((v / 10) + '0');
+				}
+				_putchar((v % 10) + '0');
 			}
-			else if (v <= 99 && v >= 10)
-				_putchar((v / 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 }
