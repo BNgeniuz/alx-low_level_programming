cd7de1ea97a1a5a502235a6ca92879a027a555df
BNgeniuz <godwin.bruce@gmail.com>
2023-07-19 01:24:06 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point, fibonacci
+ * Return: (0) prints fibonacci num
+ */
+int main(void)
+{
+	int q = 0;
+	unsigned long int x = 0, y = 1, z = 0;
+
+	while (q < 98)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		printf("%lu", z);
+
+		if (q < 97)
+			printf(", ");
+		q++;
+	}
+	putchar('\n');
+	return (0);
+}
