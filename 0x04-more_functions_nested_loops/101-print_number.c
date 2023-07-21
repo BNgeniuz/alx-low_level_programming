99df1dddcc45662a726331e0d4d0705b5d5fcae7
BNgeniuz <godwin.bruce@gmail.com>
2023-07-20 21:55:09 -0700

    101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - print integer num
+ * @n: num to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int tbt = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		tbt = tbt;
+	}
+	if ((tbt / 10) > 0)
+		print_number(tbt / 10);
+
+	_putchar((tbt % 10) + '0');
+}
