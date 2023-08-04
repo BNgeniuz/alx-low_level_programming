9a5966fc71d0d234e5b0eb8c50d61d1fdb3d6c73
BNgeniuz <godwin.bruce@gmail.com>
2023-08-03 17:44:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Printing of its names followed by newline
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 0 (whatsmyname)
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_HEADER_
+#define MAIN_HEADER_
+
+int _putchar(char c);
+int main (int argc, char *argv[]);
+
+#endif
