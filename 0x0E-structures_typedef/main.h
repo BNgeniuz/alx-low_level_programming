199b87214c8d6f9a0b6b3113939db6e0c7d49541
BNgeniuz <godwin.bruce@gmail.com>
2023-08-14 06:45:52 -0700

    header file

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_HEADER_
+#define MAIN_HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(void);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
