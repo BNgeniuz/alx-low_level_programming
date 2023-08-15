3310c90814010243811d58fadc389112ca359b33
BNgeniuz <godwin.bruce@gmail.com>
2023-08-14 18:15:53 -0700

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_header
+#define dog_header
+
+/**
+ * struct dog - dog features
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Return: 0 (snoop dog)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
