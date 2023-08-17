1d780d14329ad15dded6c1abfd781a8ee3c9f41c
BNgeniuz <godwin.bruce@gmail.com>
2023-08-17 07:10:09 -0700

    Write a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(args, int));
 
-			if (x != x - 1 || separator != NULL)
+			if (x != x - 1 && separator != NULL)
 				printf("%s", separator);
 
 			x++;
