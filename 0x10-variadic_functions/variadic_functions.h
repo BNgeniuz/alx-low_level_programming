bd3a465c1109aa8c8ffe603735ccd819c0def489
BNgeniuz <godwin.bruce@gmail.com>
2023-08-17 11:18:05 -0700

    variadic header files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef _header_
-#define _header_
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+#include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -11,15 +12,4 @@ void print_a_char(char *separator, va_list args);
 void print_a_integer(char *separator, va_list args);
 void print_a_float(char *separator, va_list args);
 void print_a_char_ptr(char *separator, va_list args);
-
-/**
- * struct format_types - Struct format_types
- * @identifier: The conversion specifier
- * @f: The function pointer
- */
-typedef struct format_types
-{
-	char *identifier;
-	void (*f)(char *separator, va_list args);
-} f_dt;
 #endif
