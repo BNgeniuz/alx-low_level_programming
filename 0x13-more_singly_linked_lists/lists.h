a48d3a49407ebb42bd0c1fe4f6989b65370f3ff5
Godwin Bruce <godwin.bruce@gmail.com>
2023-08-29 16:28:12 -0700

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,21 +20,6 @@ typedef struct listint_s
 } listint_t;
 
 
-/**
- * struct listp_s - singly linked list used.
- * @f: node pointers
- * @next: next node.
- *
- * Description: singly linked pointers list.
- */
-typedef struct listp_s
-{
-	void *f;
-	struct listp_s *next;
-} listp_t;
-
-
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -50,5 +35,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int _putchar(char c);
 
 #endif
