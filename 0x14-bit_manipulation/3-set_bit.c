9241d48c2171e895265a0daaccca3a08daac3171
Godwin Bruce <godwin.bruce@gmail.com>
2023-10-26 19:13:55 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,16 +3,18 @@
 #include <stdio.h>
 
 /**
- * print_binary - function that prints the binary representation of a number
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: integer input num
+ * @index: index value returns
  *
- * @n: binary to input
- *
- * Return: 0 (binary prints)
+ * Return: 1 (ssucess  or -1, failure)
  */
 
-void print_binary(unsigned long int n)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
